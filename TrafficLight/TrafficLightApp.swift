15a6ac20ef82b3ca8345986b4265503fc231bdef
Aleksandr Matukin <aleksandr.matukin@icloud.com>
2022-01-21 12:53:27 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Александр Матукин on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
