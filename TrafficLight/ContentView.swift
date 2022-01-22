fa8636da461127f4303959423df6bd8f26723e43
Aleksandr Matukin <aleksandr.matukin@icloud.com>
2022-01-22 20:46:00 +0300

    HW

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
                 .cornerRadius(15)
                 .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white,lineWidth: 3))
                 .foregroundColor(.white)
+                .padding()
             }
         }
     }
